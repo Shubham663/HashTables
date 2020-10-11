fa8e96f6e800e0ffcd42fb9b474ee012d7bc701a
unknown <mittalshubham05@gmail.com>
2020-10-12 01:59:46 +0530

    Create: created functionality for finding frequency of words in a sentence using hashtable

---
+++ src/main/java/com/bridgelabz/hashtables/MainClass.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.hashtables;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class MainClass 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Linked List Operations!" );
+        
+    }
+}
